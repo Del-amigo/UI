15354ec9a3a2497c7cda2e0972f200843623f8cf
Del-amigo <64439373+Del-amigo@users.noreply.github.com>
2020-08-14 22:55:50 -0400

    LastChanges

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,20 +1,45 @@
 function clicked() {
-	document.getElementById('lblError').innerHTML = "";
-  var uname = document.getElementById("email").value;
-  var pwd = document.getElementById("pwd1").value;
-
+  document.getElementById('lblError').innerHTML = '';
+  var uname = document.getElementById('email').value;
+  var pwd = document.getElementById('pwd1').value;
   var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  if (uname == '') {
-	document.getElementById('lblError').innerHTML = "** Please enter Userame";
+
+  if (uname == '' && pwd == '') {
+    document.getElementById('lblError').innerHTML = ' ** Please enter Username and Password';
+  } else if (uname == '') {
+    document.getElementById('lblError').innerHTML = ' ** Please enter Username';
   } else if (pwd == '') {
-   document.getElementById('lblError').innerHTML = "** Please enter Password";
+    document.getElementById('lblError').innerHTML = ' ** Please enter Password';
   } else if (!filter.test(uname)) {
-    alert("** Enter valid email");
-  } else if (uname != "technobank" && pwd != 123456789) {
-    alert("Invalid credentials, please provide correct username and password");
+    document.getElementById('lblError').innerHTML = ' ** Please provide @gmail.com';
+  } else if (uname != 'technobank' && pwd != 12345678) {
+    document.getElementById('lblError').innerHTML = ' ** // Invalid password';
   } else if (pwd.length < 2 || pwd.length > 10) {
-    alert("Password min and max length is 9.");
+    document.getElementById('lblError').innerHTML = ' ** Password min and max length is 9';
   } else {
-    window.location = "Hompage.html";
+    window.location = 'ACCOUNT SUMMARY PAGE.html';
   }
 }
+
+// function validateUserName(email) {
+//            var regex = /^[0-9a-zA-Z_]+$/;
+//            if (name.length < 8)
+//                document.getElementById("msg1").innerHTML = "Username is too short";
+//            else if (name.length > 15)
+//                document.getElementById("msg1").innerHTML = "Username is too long";
+//            else if (!name.match(regex)) {
+//                document.getElementById("msg1").innerHTML = "Invalid characters in Username";
+//            } else document.getElementById("msg1").innerHTML = "";
+//                return;
+//        } // end function validateUserName
+
+//        function validatePassword(pwd1) {
+//            var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+//            if (password.length < 8)
+//                document.getElementById("msg2").innerHTML = "Password is too short (min. 8 characters)";
+//            else if (password.length > 15)
+//                document.getElementById("msg2").innerHTML = "Password is too long (max. 15 characters)";
+//            else if (!password.match(regex)) {
+//                document.getElementById("msg2").innerHTML = "Invalid Password";
+//            } else document.getElementById("msg2").innerHTML = "";
+//                return;
